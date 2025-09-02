6514c1a89d6f2b1abe08060e69bb7f97c2a0960e
sushmapalle <su.palle@ufl.edu>
2025-09-02 15:42:43 -0400

    Delete datatags directory
    Cleanup: remove unused datatags directory

--- datatags/app.cgi
+++
@@ -1,6 +0,0 @@
-from wsgiref.handlers import CGIHandler
-
-from app import create_app
-application = create_app()
-
-CGIHandler().run(application)
--- datatags/hello1.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# ^ this must be first line, no spaces, no BOM
-import sys
-print("Content-Type: text/plain; charset=utf-8")
-print()  # blank line after headers
-print("HELLO")
