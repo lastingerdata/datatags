74f06dc86e9cc85ecc8c01c69650e3453e8c3fe4
Probably Sushma <lastingerdatasupport@ufl.edu>
2025-11-25 13:33:38 -0500

    updating tags_index file

---
+++ ufl_tag_manager/tags_index.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os, cgitb
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+cgitb.enable()
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+TEMPLATES = os.path.join(ROOT, "templates")
+
+env = Environment(
+    loader=FileSystemLoader(TEMPLATES),
+    autoescape=select_autoescape(["html","xml"]),
+)
+
+def main():
+    html = env.get_template("tags_index.html").render(
+        base_path="/ufl_tag_manager",
+        ext=".py ",   
+        user=os.environ.get("REMOTE_USER", "unknown"),
+    )
+    print("Content-Type: text/html; charset=utf-8")
+    print()
+    print(html)
+
+if __name__ == "__main__":
+    main()
