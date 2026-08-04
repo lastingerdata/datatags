1d0add70284007de362d9b117fa90e09d2db916d
Probably Sushma <lastingerdatasupport@ufl.edu>
2026-08-04 14:42:03 -0400

    code cleanup

--- ufl_tag_manager/documentation.py
+++ ufl_tag_manager/documentation.py
@@ -57,35 +57,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-# #!/usr/bin/env python3
-# import os, cgitb
-# from jinja2 import Environment, FileSystemLoader, select_autoescape
-
-# from env_config import get_base_path
-
-# cgitb.enable()
-
-# ROOT = os.path.dirname(os.path.abspath(__file__))
-# TEMPLATES = os.path.join(ROOT, "templates")
-
-# env = Environment(
-#     loader=FileSystemLoader(TEMPLATES),
-#     autoescape=select_autoescape(["html","xml"]),
-# )
-
-# def main():
-#     html = env.get_template("documentation.html").render(
-#         base_path=get_base_path(),
-#         ext=".py",
-#         user=os.environ.get("REMOTE_USER", "unknown"),
-#     )
-#     print("Content-Type: text/html; charset=utf-8")
-#     print()
-#     print(html)
-
-# if __name__ == "__main__":
-#     main()
